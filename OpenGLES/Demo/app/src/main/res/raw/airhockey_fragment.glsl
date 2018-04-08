a58e8543e29b37ef3a8e848aa2e4b1405dd2a884
zhusonger <449350247@qq.com>
2018-04-08 21:53:05 +0800

    删除OpenGL相关的

--- OpenGLES/Demo/app/src/main/res/raw/airhockey_fragment.glsl
+++
@@ -1,7 +0,0 @@
-precision float medium;
-
-uniform vec4 uColor;
-
-void main() {
-    gl_FragColor = uColor;
-}
--- OpenGLES/Demo/app/src/main/res/raw/airhockey_vertex.glsl
+++
@@ -1,6 +0,0 @@
-attribute vec4 aPosition;
-
-void main() {
-    gl_Position = aPosition;
-    gl_PointSize = 10.0;
-}
